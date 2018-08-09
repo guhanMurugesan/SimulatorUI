12eb48ec5994b582b4a4d8a689efa170ab190f90
guhan murugesan <mguhan.rmd@gmail.com>
2018-08-09 09:53:12 +0530

    init commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
+import { CoreComponent } from './features/core/core.component';
+import { ConsoleComponent } from './features/console/console.component';
+import { MenuComponent } from './features/menu/menu.component';
+import { HeaderComponent } from './features/header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CoreComponent,
+    ConsoleComponent,
+    MenuComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule
